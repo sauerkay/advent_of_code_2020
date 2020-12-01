f5eba5ae4c4084e76d3994de0abdc955031b7cd5
Daniel Krause <dkrause@sauermbp.local>
2020-12-01 11:46:13 -0700

    comp[leted problem 2 and cleaned up variable names

--- dec_01/dec_01.go
+++ dec_01/dec_01.go
@@ -14,24 +14,38 @@ func main() {
 		log.Fatal(err)
 	}
 
-	testData := strings.Split(string(input), "\n")
+	data := strings.Split(string(input), "\n")
 
-	for i := 0; i < len(testData); i++ {
-		for x := 0; x < len(testData); x++ {
-			if testData[i] != testData[x] && testData[i] != "" && testData[x] != "" {
-				a, err := strconv.Atoi(testData[i])
+	for x := 0; x < len(data); x++ {
+		for y := 0; y < len(data); y++ {
+			if data[x] != data[y] && data[x] != "" && data[y] != "" {
+				a, err := strconv.Atoi(data[x])
 				if err != nil {
 					log.Fatal(err)
 				}
-				b, err := strconv.Atoi(testData[x])
+				b, err := strconv.Atoi(data[y])
 				if err != nil {
 					log.Fatal(err)
 				}
 				calc := a + b
 				if calc == 2020 {
-					fmt.Println(testData[i], " + ", testData[x], " = ", calc)
+					fmt.Println(data[x], " + ", data[y], " = ", calc)
 					multiply := a * b
 					fmt.Println(multiply)
+				}
+				for z := 0; z < len(data); z++ {
+					if data[z] != data[x] && data[z] != data[y] && data[z] != "" {
+						c, err := strconv.Atoi(data[z])
+						if err != nil {
+							log.Fatal(err)
+						}
+						calc_2 := calc + c
+						if calc_2 == 2020 {
+							fmt.Println(data[x], " + ", data[y], "+", data[z], " = ", calc_2)
+							multiply_2 := a * b * c
+							fmt.Println(multiply_2)
+						}
+					}
 				}
 			}
 		}
