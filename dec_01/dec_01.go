ba46298a8ab1af1db235cc51e83d797a9c5653d4
Daniel Krause <dkrause@sauermbp.local>
2020-12-01 11:21:38 -0700

    cleaned up final code

--- dec_01/dec_01.go
+++ dec_01/dec_01.go
@@ -19,19 +19,15 @@ func main() {
 	for i := 0; i < len(testData); i++ {
 		for x := 0; x < len(testData); x++ {
 			if testData[i] != testData[x] && testData[i] != "" && testData[x] != "" {
-				// fmt.Println(testData[i])
 				a, err := strconv.Atoi(testData[i])
 				if err != nil {
 					log.Fatal(err)
 				}
-				// fmt.Println(testData[x])
 				b, err := strconv.Atoi(testData[x])
 				if err != nil {
 					log.Fatal(err)
 				}
-				// return a, b
 				calc := a + b
-				// fmt.Println(calc)
 				if calc == 2020 {
 					fmt.Println(testData[i], " + ", testData[x], " = ", calc)
 					multiply := a * b
